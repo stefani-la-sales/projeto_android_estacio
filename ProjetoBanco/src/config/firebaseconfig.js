914af331f2337b70da8a30bc9d5f0e8d0614d7f3
stefani.la.sales <stefani.la.sales@gmail.com>
2022-04-25 02:49:26 -0300

    terceiro

--- ProjetoBanco/src/config/firebaseconfig.js
+++ ProjetoBanco/src/config/firebaseconfig.js
@@ -1,5 +1,7 @@
-import firebase from 'firebase';
+import {initializeApp} from 'firebase/app';
+import {getAnalytics} from 'firebase/analytics';
 import "firebase/storage";
+import firebase from 'firebase'
 
 const firebaseConfig = {
     apiKey: "AIzaSyCUpzesrqrs1U601FZvDYp181ubp9X1USg",
@@ -10,8 +12,6 @@ const firebaseConfig = {
     appId: "1:709226666821:web:1f7a8273cc44f2e6b83c2c",
     measurementId: "G-CZ6B3WDEMF"
 };
-
-firebase.initializeApp(firebaseConfig);
-
+initializeApp(firebaseConfig);
 const database = firebase.firestore();
 export default database;
--- ProjetoBanco/src/pages/home/index.js
+++ ProjetoBanco/src/pages/home/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+    Text,
+    View,
+    TouchableOpacity,
+    FlatList
+} from 'react-native';
+import styles from '../../assests/style';
+import database from '../../config/firebaseconfig';
 
-export default function HomePage() {
+
+export default function HomePage({ navigation }) {
+    const [item, setItem] = useState([]);
+
+    useEffect(() => {
+        database.collection("Banco").onSnapshot((query) => {
+            const list = [];
+            query.array.forEach((doc) => {
+                list.push({ ...doc.data(), id: doc.id });
+            });
+            setItem(list);
+        });
+    }, []);
+
     return (
-        <View>
-            <Text>Oláááá</Text>
+        <View style={styles.background}>
+            <FlatList />
+            <TouchableOpacity>
+                <Text>Oiii</Text>
+            </TouchableOpacity>
         </View>
     )
 }
